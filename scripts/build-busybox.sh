d6796213e78b9681d898e84bcdf67e98f599a35b
sfiraz <hermawanwildan78@gmail.com>
2025-11-23 06:44:22 +0000

    update busybox!

--- scripts/build-busybox.sh
+++ scripts/build-busybox.sh
@@ -9,9 +9,15 @@ wget -q https://busybox.net/downloads/busybox-1.36.1.tar.bz2
 tar -xf busybox-1.36.1.tar.bz2
 cd busybox-1.36.1
 
+# Gunakan oldconfig untuk non-interactive build
 make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- defconfig
 
+# Set config secara non-interactive
 sed -i 's/.*CONFIG_STATIC.*/CONFIG_STATIC=y/' .config
+sed -i 's/.*CONFIG_FEATURE_USE_INITTAB.*/CONFIG_FEATURE_USE_INITTAB=n/' .config
+
+# Apply changes tanpa prompt interaktif
+yes "" | make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- oldconfig
 
 make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- -j$(nproc)
 make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- install
