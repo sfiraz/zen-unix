36e474f3acd79351a68ff339e9100351e327c365
Wildan Hermawan <hermawanwildan78@gmail.com>
2025-11-23 21:03:33 +0700

    Update install-tools.sh

--- scripts/install-tools.sh
+++ scripts/install-tools.sh
@@ -5,37 +5,52 @@ echo "Installing additional tools..."
 
 cd toolchain
 
-# Download dan install Go
+# Install Go dengan error handling
 echo "Installing Go..."
-wget -q https://go.dev/dl/go1.25.4.linux-arm64.tar.gz
-tar -xf go1.25.4.linux-arm64.tar.gz
-cp -r go ../rootfs/rootfs.dir/usr/
+if wget -q https://go.dev/dl/go1.25.4.linux-arm64.tar.gz; then
+    tar -xf go1.25.4.linux-arm64.tar.gz
+    cp -r go ../rootfs/rootfs.dir/usr/
+else
+    echo "Go download failed, skipping..."
+fi
 
-# Download dan install GCC (C/C++ compiler)
+# Install GCC
 echo "Installing GCC..."
-wget -q http://fl.us.mirror.archlinuxarm.org/aarch64/core/gcc-14.2.0-1-aarch64.pkg.tar.xz
-tar -xf gcc-*.pkg.tar.xz
-cp -r usr ../rootfs/rootfs.dir/
+if wget -q http://fl.us.mirror.archlinuxarm.org/aarch64/core/gcc-14.2.0-1-aarch64.pkg.tar.xz; then
+    tar -xf gcc-*.pkg.tar.xz
+    cp -r usr ../rootfs/rootfs.dir/
+else
+    echo "GCC download failed, skipping..."
+fi
 
-# Download dan install Git
+# Install Git
 echo "Installing Git..."
-wget -q http://fl.us.mirror.archlinuxarm.org/aarch64/core/git-2.49.1-1-aarch64.pkg.tar.xz
-tar -xf git-*.pkg.tar.xz
-cp -r usr ../rootfs/rootfs.dir/
+if wget -q http://fl.us.mirror.archlinuxarm.org/aarch64/core/git-2.49.1-1-aarch64.pkg.tar.xz; then
+    tar -xf git-*.pkg.tar.xz
+    cp -r usr ../rootfs/rootfs.dir/
+else
+    echo "Git download failed, skipping..."
+fi
 
-# Download dan install zlib
+# Install zlib
 echo "Installing zlib..."
-wget -q http://fl.us.mirror.archlinuxarm.org/aarch64/core/zlib-1:1.3.1-2-aarch64.pkg.tar.xz
-tar -xf zlib-*.pkg.tar.xz
-cp -r usr ../rootfs/rootfs.dir/
+if wget -q http://fl.us.mirror.archlinuxarm.org/aarch64/core/zlib-1:1.3.1-2-aarch64.pkg.tar.xz; then
+    tar -xf zlib-*.pkg.tar.xz
+    cp -r usr ../rootfs/rootfs.dir/
+else
+    echo "zlib download failed, skipping..."
+fi
 
-# Download dan install Neovim (editor)
+# Install Neovim
 echo "Installing Neovim..."
-wget -q https://github.com/neovim/neovim/releases/download/stable/nvim-linux-arm64.tar.gz
-tar -xf nvim-linux-arm64.tar.gz
-cp -r nvim-linux-arm64 ../rootfs/rootfs.dir/usr/
+if wget -q https://github.com/neovim/neovim/releases/download/stable/nvim-linux-arm64.tar.gz; then
+    tar -xf nvim-linux-arm64.tar.gz
+    cp -r nvim-linux-arm64 ../rootfs/rootfs.dir/usr/
+else
+    echo "Neovim download failed, skipping..."
+fi
 
 # Cleanup
-rm -rf *.tar.* *.pkg.* usr go
+rm -rf *.tar.* *.pkg.* usr go 2>/dev/null || true
 
 echo "Tools installation complete"
